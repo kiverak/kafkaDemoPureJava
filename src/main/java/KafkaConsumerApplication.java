d6c0aed39a1bd7b711b9a3fe5eadcd35c4dbd2b5
vitalii.b <kiverak@gmail.com>
2025-06-25 22:37:09 +0300

    records for group

--- src/main/java/KafkaConsumerApplication.java
+++ src/main/java/KafkaConsumerApplication.java
@@ -1,4 +1,5 @@
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
@@ -7,8 +8,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
+import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
+import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
 
 public class KafkaConsumerApplication {
@@ -22,9 +25,17 @@ public class KafkaConsumerApplication {
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 //        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
+        // если не указать id инстанса группы, он будет генерироваться каждый раз
+        // и будет происходить перебалансировка кластера
+        properties.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "my-group-instance-id");
 
+//        getRecords(properties);
+        getRecordsForGroup(properties);
+    }
+
+    private static void getRecords(Properties properties) {
         try (var consumer = new KafkaConsumer<String, String>(properties)) {
-//            consumer.subscribe(Arrays.asList(properties.getProperty("topic")));
             consumer.assign(List.of(
                     new TopicPartition("sandbox", 0),
                     new TopicPartition("sandbox", 1),
@@ -37,5 +48,32 @@ public class KafkaConsumerApplication {
                         .forEach(record -> logger.info("Record: {}", record));
             }
         }
+    }
+
+    private static void getRecordsForGroup(Properties properties) {
+        try (var consumer = new KafkaConsumer<String, String>(properties)) {
+            consumer.subscribe(Pattern.compile("sandbox"), new MyConsumerRebalanceListener());
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                StreamSupport.stream(records.spliterator(), false)
+                        .forEach(record -> logger.info("Record: {}", record));
+            }
+        }
+    }
+}
+
+class MyConsumerRebalanceListener implements ConsumerRebalanceListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyConsumerRebalanceListener.class);
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        logger.info("Partitions revoked: {}", partitions);
+    }
+
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        logger.info("Partitions assigned: {}", partitions);
     }
 }
