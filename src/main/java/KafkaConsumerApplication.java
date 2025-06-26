3d54520eddbb82c8071a1bdf1f6054898d520728
vitalii.b <kiverak@gmail.com>
2025-06-26 17:58:52 +0300

    seek offsets

--- src/main/java/KafkaConsumerApplication.java
+++ src/main/java/KafkaConsumerApplication.java
@@ -2,6 +2,8 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -10,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
@@ -24,26 +27,34 @@ public class KafkaConsumerApplication {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092,localhost:39092,localhost:49092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-//        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+//        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "my-group-id");
         // если не указать id инстанса группы, он будет генерироваться каждый раз
         // и будет происходить перебалансировка кластера
-        properties.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "my-group-instance-id");
+//        properties.setProperty(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "my-group-instance-id");
 
-//        getRecords(properties);
-        getRecordsForGroup(properties);
+        getRecords(properties);
+//        getRecordsForGroup(properties);
     }
 
     private static void getRecords(Properties properties) {
         try (var consumer = new KafkaConsumer<String, String>(properties)) {
             consumer.assign(List.of(
-                    new TopicPartition("sandbox", 0),
-                    new TopicPartition("sandbox", 1),
-                    new TopicPartition("sandbox", 2)
+//                    new TopicPartition("sandbox", 0),
+                    new TopicPartition("sandbox", 1)
+//                    new TopicPartition("sandbox", 2)
             ));
 
+//            consumer.seek(new TopicPartition("sandbox", 1), new OffsetAndMetadata(5));
+//            consumer.seekToEnd(List.of(new TopicPartition("sandbox", 1)));
+            var offsets = consumer.offsetsForTimes(Map.of(new TopicPartition("sandbox", 1), 1750878518535L));
+            consumer.seek(
+                    new TopicPartition("sandbox", 1),
+                    offsets.get(new TopicPartition("sandbox", 1)).offset()
+            );
+
             while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
                 StreamSupport.stream(records.spliterator(), false)
                         .forEach(record -> logger.info("Record: {}", record));
             }
